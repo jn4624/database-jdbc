0e5d919921ad01694cbeecc085ac304f6d1aed69
LimJiNa <jn4624@gmail.com>
2024-04-06 22:15:39 +0900

    JDBC - 조회 기능 구현 및 테스트

--- src/main/java/hello/jdbc/domain/Member.java
+++ src/main/java/hello/jdbc/domain/Member.java
@@ -7,6 +7,10 @@ public class Member {
     private String memberId;
     private int money;
 
+    public Member() {
+
+    }
+
     public Member(String memberId, int money) {
         this.memberId = memberId;
         this.money = money;
