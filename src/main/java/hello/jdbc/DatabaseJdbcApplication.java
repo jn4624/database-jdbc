86005ed354fa6983cf831ff18eb742185b663d18
LimJiNa <jn4624@gmail.com>
2024-04-06 15:31:52 +0900

    Initial commit

---
+++ src/main/java/hello/jdbc/DatabaseJdbcApplication.java
@@ -0,0 +1,13 @@
+package hello.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseJdbcApplication.class, args);
+	}
+
+}
